d2ed61255574f2e4833a5cc6c2cf2eda1324e4ad
keyem4251 <kisk.emura@gmail.com>
2022-03-29 22:14:31 +0900

    command line gcd

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,24 @@
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new()
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m)
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d)
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
@@ -10,7 +29,7 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
             m = n;
             n = t;
         }
-        m = m % n;
+        m = % n;
     }
     n
 }
@@ -18,5 +37,5 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 #[test]
 fn test_gcd() {
     assert_eq!(gcd(14, 15), 1);
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11)
 }
