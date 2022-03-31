7677052dbd2f0d10a56a95128d5307dbbb90bfc7
keyem4251 <kisk.emura@gmail.com>
2022-03-31 21:15:17 +0900

    add escape time

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,14 +1,18 @@
 extern crate num;
+
 use num::Complex;
 
 fn main() {
     println!("Hello, world!");
 }
 
-#[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>) {
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
+    for i in 0..limit {
         z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
     }
+    None
 }
