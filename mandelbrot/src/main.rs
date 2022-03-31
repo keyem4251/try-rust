9b6f3769e32e4c2e92e44a3f7dda620f01da3b33
keyem4251 <kisk.emura@gmail.com>
2022-03-31 21:45:27 +0900

    add render fn and change type

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -63,15 +63,15 @@ fn pixel_to_point(
     pixel: (usize, usize),
     upper_left: Complex<f64>,
     lower_right: Complex<f64>,
-) -> Complex<u64> {
+) -> Complex<f64> {
     let (width, height) = (
         lower_right.re - upper_left.re,
         upper_left.im - lower_right.im,
     );
 
     Complex {
-        re: (upper_left.re + pixel.0 as f64 * width / bounds.0 as f64) as u64,
-        im: (upper_left.im - pixel.1 as f64 * height / bounds.1 as f64) as u64,
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
     }
 }
 
@@ -86,4 +86,23 @@ fn test_pixel_to_point() {
         ),
         Complex { re: -0.5, im: -0.5 }
     )
+}
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert_eq!(pixels.len(), bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
 }
