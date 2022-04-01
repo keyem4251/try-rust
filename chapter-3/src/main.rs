f24fb7e0da05fbcda10c017b8e75a0bae92587e2
keyem4251 <kisk.emura@gmail.com>
2022-04-01 21:27:28 +0900

    add overflow wrap

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("{:?}", build_vector());
+    println!("{}", int_wrap());
 }
 
 fn build_vector() -> Vec<i16> {
@@ -7,4 +8,10 @@ fn build_vector() -> Vec<i16> {
     v.push(20);
     v.push(10);
     v
+}
+
+fn int_wrap() -> i32 {
+    let big_val = i32::MAX;
+    let x = big_val.wrapping_add(1);
+    x
 }
