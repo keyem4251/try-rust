e928c025969ebbebce82bc3a63519f4909000985
keyem4251 <kisk.emura@gmail.com>
2022-04-01 20:10:26 +0900

    add build_vector fn

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", build_vector());
+}
+
+fn build_vector() -> Vec<i16> {
+    let mut v = Vec::new();
+    v.push(20);
+    v.push(10);
+    v
 }
