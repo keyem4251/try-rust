4a6cbf48a62622accf1b99cae9fcde95dfeea941
keyem4251 <kisk.emura@gmail.com>
2022-04-02 12:13:16 +0900

    cast example

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("{:?}", build_vector());
     println!("{}", int_wrap());
+    println!("{:?}", cast_int());
 }
 
 fn build_vector() -> Vec<i16> {
@@ -14,4 +15,22 @@ fn int_wrap() -> i32 {
     let big_val = i32::MAX;
     let x = big_val.wrapping_add(1);
     x
+}
+
+fn cast_int() {
+    assert_eq!(10_i8 as u16, 10_u16);
+    assert_eq!(2525_u16 as i16, 2525_i16);
+
+    assert_eq!(-1_i16 as i32, -1_i32);
+    assert_eq!(65535_u16 as i32, 65535_i32);
+
+    assert_eq!(1000_i16 as u8, 232_u8);
+    assert_eq!(65535_u32 as i16, -1_i16);
+
+    assert_eq!(-1_i8 as u8, 255_u8);
+    assert_eq!(255_u8 as i8, -1_i8);
+
+    assert_eq!(2u16.pow(4), 16); // 2の4乗
+    assert_eq!((-4i32).abs(), 4); // 絶対値
+    assert_eq!(0b101101u8.count_ones(), 4)
 }
