de44b23022ad32ee6414dd51cf56f4422ee196f2
keyem4251 <kisk.emura@gmail.com>
2022-04-03 17:00:06 +0900

    add lsit fn

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -75,3 +75,11 @@ fn pointer_() {
     let t = (12, "eggs");
     let _b = Box::new(t); // allocate a tuple in the heap
 }
+
+fn list_() {
+    let lazy_caterer: [u32; 6] = [1, 2, 4, 7, 11, 16];
+    let taxonomy = ["Animalia", "Arthropoda", "Insecta"];
+
+    assert_eq!(lazy_caterer[3], 7);
+    assert_eq!(taxonomy.len(), 3);
+}
