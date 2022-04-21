11b8ee09602d1a028b9068b8352613bd531eb156
keyem4251 <kisk.emura@gmail.com>
2022-04-21 21:33:31 +0900

    add section 6.2

--- chapter-6/src/main.rs
+++ chapter-6/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     section_6_1();
+    section_6_2();
 }
 
 fn section_6_1() {
@@ -8,5 +9,28 @@ fn section_6_1() {
         println!("{:?}", i);
     } else {
         println!("if expression");
+    };
+}
+
+fn section_6_2() {
+    let i = 10;
+    let display_name = match i > 5 {
+        Some(b) => b,
+        None => {
+            let s = 10;
+            s
+        }
+    };
+
+    let msg = {
+        let mut dandelion_control = 10;
+        dandelion_control += 20;
+        dandelion_control
+    };
+
+    let msg2 = {
+        let a = 10;
+        ;
+        a
     };
 }
